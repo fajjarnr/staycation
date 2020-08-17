9f6ee110bfd88a771edd9ef0181559e56e6689fc
fajjarnr <zfajart@gmail.com>
2020-08-17 21:09:54 +0700

    Updates

---
+++ precache-manifest.d83f33039880ed0f3d2fd2031609c9e7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7381fd85c3545a1ecf3d2241a626b47d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ec04f9e47382c56ea3b",
+    "url": "/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "a8190227bfc53d56fc9a",
+    "url": "/static/css/main.df36a0ba.chunk.css"
+  },
+  {
+    "revision": "8ec04f9e47382c56ea3b",
+    "url": "/static/js/2.28eb2cf7.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/static/js/2.28eb2cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8190227bfc53d56fc9a",
+    "url": "/static/js/main.3e365cf6.chunk.js"
+  },
+  {
+    "revision": "51c34589efe3186435c0",
+    "url": "/static/js/runtime-main.06a42d8b.js"
+  },
+  {
+    "revision": "67e25a39beb992b5476840cc2ce5aa21",
+    "url": "/static/media/calendar.67e25a39.svg"
+  },
+  {
+    "revision": "bc95ea7081dab6207da118aa85e6c3fa",
+    "url": "/static/media/cities.bc95ea70.svg"
+  },
+  {
+    "revision": "c6ccd4740c8b875337693a033d847944",
+    "url": "/static/media/completed.c6ccd474.jpg"
+  },
+  {
+    "revision": "19fa22a679fb41aa197b518e0eb2d802",
+    "url": "/static/media/img-hero-frame.19fa22a6.jpg"
+  },
+  {
+    "revision": "97bfd6d983540d6604427f3bf547c0a5",
+    "url": "/static/media/img-hero.97bfd6d9.jpg"
+  },
+  {
+    "revision": "e1f52ffd6cefde4614a8547417fb990d",
+    "url": "/static/media/logo-bca.e1f52ffd.jpg"
+  },
+  {
+    "revision": "8c44d8bad4f296823c1f70bb36bfc4c6",
+    "url": "/static/media/logo-mandiri.8c44d8ba.jpg"
+  },
+  {
+    "revision": "11a62c712d17eaee33a02927afd01988",
+    "url": "/static/media/star.11a62c71.svg"
+  },
+  {
+    "revision": "83252578f7d1d78cfd88b9fb0264f4ef",
+    "url": "/static/media/testimonial-landingpages-frame.83252578.jpg"
+  },
+  {
+    "revision": "109e18e8160fb2c8887bfc75e9cbc2c6",
+    "url": "/static/media/traveler.109e18e8.svg"
+  },
+  {
+    "revision": "bb259d86e7904d0ce5931ebee9924b69",
+    "url": "/static/media/treasure.bb259d86.svg"
+  }
+]);
