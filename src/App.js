f00bc319d2862bd9b13f56ffa5851d4a524008d2
fajjarnr <zfajart@gmail.com>
2022-01-08 21:01:00 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import LandingPage from 'pages/LandingPage';
+import DetailsPage from 'pages/DetailsPage';
+import Checkout from 'pages/Checkout';
+import Example from 'pages/Example';
+import NotFound from 'pages/404';
 
-import "./assets/scss/style.scss";
-import LandingPage from "pages/LandingPage";
-import DetailPage from "pages/DetailPage";
-import Checkout from "pages/Checkout";
+import 'assets/scss/style.scss';
 
-import InputNumber from "pages/InputNumber";
-import InputDate from "pages/InputDate";
-import Breadcrumb from "pages/Breadcrumb";
+const history = createBrowserHistory({
+  basename: process.env.PUBLIC_URL,
+});
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Route exact path="/" component={LandingPage}></Route>
-        <Route exact path="/properties/:id" component={DetailPage}></Route>
-        <Route path="/checkout" component={Checkout}></Route>
-
-        <Route exact path="/inputnumber" component={InputNumber}></Route>
-        <Route exact path="/inputdate" component={InputDate}></Route>
-        <Route exact path="/breadcrumb" component={Breadcrumb}></Route>
+      <Router history={history} basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/properties/:id" component={DetailsPage} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/example" component={Example} />
+          <Route path="*" component={NotFound} />
+        </Switch>
       </Router>
+
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
